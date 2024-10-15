07ceae08f5783bb852d275701c6bff40853549cd
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-10-15 13:04:34 -0400

    Generates component Banks, while adding selector to the root component and import statement to root componenet's imports array.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BanksComponent} from './banks/banks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BanksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/banks/banks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banks',
+  standalone: true,
+  imports: [],
+  templateUrl: './banks.component.html',
+  styleUrl: './banks.component.css'
+})
+export class BanksComponent {
+
+}
